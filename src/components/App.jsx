d494751f93727d1d94ce749dd239100a0d57aa23
moonye@live.fr <moonye@live.fr>
2025-03-18 16:06:43 +0100

    ADD : Fini Partie 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,22 @@
-import Banner from "./Banner";
-import logo from "../assets/logo.png";
+import Banner from './Banner'
+import logo from '../assets/logo.png'
 // import Cart from './Cart'
-import ShoppingList from "./ShoppingList";
+import Footer from './Footer'
+import ShoppingList from './ShoppingList'
 
 function App() {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    const inputValue = formData.get('my_input');
-    console.log('Formulaire soumis avec:', inputValue);
-    // Traitez les données du formulaire ici
-  };
+	return (
+		<div>
+			<Banner>
+				<img src={logo} alt='La maison jungle' className='lmj-logo' />
+				<h1 className='lmj-title'>La maison jungle</h1>
+			</Banner>
 
-  return (
-    <div>
-      <Banner>
-        <img src={logo} alt="La maison jungle" className="lmj-logo" />
-        <h1 className="lmj-title">La maison jungle</h1>
-      </Banner>
-      {/* <Cart /> */}
-      <ShoppingList />
-
-      <form onSubmit={handleSubmit}>
-        <input type='text' name='my_input' defaultValue='Tapez votre texte' />
-        <button type='submit'>Entrer</button>
-      </form>
-    </div>
-  );
+			{/* <Cart /> */}
+			<ShoppingList />
+			<Footer />
+		</div>
+	)
 }
 
-export default App;
+export default App
