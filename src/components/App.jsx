1d711d45d30716283bfe9518763b53a21c39ab80
moonye@live.fr <moonye@live.fr>
2025-03-18 15:22:17 +0100

    ADD : suite

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,32 @@
-import Banner from './Banner'
+import Banner from "./Banner";
+import logo from "../assets/logo.png";
 // import Cart from './Cart'
-import ShoppingList from './ShoppingList'
+import ShoppingList from "./ShoppingList";
 
 function App() {
-	return (
-		<div>
-			<Banner />
-			{/* <Cart /> */}
-			<ShoppingList />
-		</div>
-	)
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const inputValue = formData.get('my_input');
+    console.log('Formulaire soumis avec:', inputValue);
+    // Traitez les données du formulaire ici
+  };
+
+  return (
+    <div>
+      <Banner>
+        <img src={logo} alt="La maison jungle" className="lmj-logo" />
+        <h1 className="lmj-title">La maison jungle</h1>
+      </Banner>
+      {/* <Cart /> */}
+      <ShoppingList />
+
+      <form onSubmit={handleSubmit}>
+        <input type='text' name='my_input' defaultValue='Tapez votre texte' />
+        <button type='submit'>Entrer</button>
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
